27c18c7a4db1ab61b39add432bfba4f239825f68
QuameOphory <quameophory@yahoo.com>
2022-09-12 07:44:50 +0000

    remove program call

--- clients.py
+++ clients.py
@@ -49,17 +49,3 @@ class Client:
 
     def list_coupon(self):
         return self._coupon
-
-
-def main():
-    client = Client('Ebenezer', '', 'Ofori-Mensah', 'Male', date(1995,8,5), '', email_address='quameophory@yahoo.com')
-    # print(client.client_id)
-    # print(client)
-    coupon = Coupon()
-    coupon.set_coupon_amount(1000.00)
-    client.purchase_coupon(coupon)
-    print(client.list_coupon())
-    print(client.client_age())
-
-if __name__ == '__main__':
-    main()
